4de22c0e3e14b0e138d1223379b77d42baee369b
Shekhar <shekkharkalshetti@gmail.com>
2024-02-14 00:25:56 +0530

    added email auth

---
+++ app/login/page.jsx
@@ -0,0 +1,95 @@
+"use client";
+
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const router = useRouter();
+
+  const supabase = createClientComponentClient();
+
+  useEffect(() => {
+    async function getUser() {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      setUser(user);
+      setLoading(false);
+    }
+
+    getUser();
+  }, []);
+
+  const handleSignUp = async () => {
+    await supabase.auth.signUp({
+      email,
+      password,
+      options: {
+        emailRedirectTo: `${location.origin}/auth/callback`,
+      },
+    });
+
+    router.refresh();
+    setEmail("");
+    setPassword("");
+  };
+
+  const handleSignIn = async () => {
+    await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+    router.refresh();
+    setEmail("");
+    setPassword("");
+  };
+
+  const onLogout = async () => {
+    await supabase.auth.signOut();
+    router.refresh();
+    setUser(null);
+  };
+
+  console.log({ loading, user });
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (user) {
+    return (
+      <div>
+        <h1>Logged In!</h1>
+        <button onClick={onLogout}>Log Out</button>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <input
+        type="email"
+        name="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        name="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleSignUp}>Sign Up</button>
+      <button onClick={handleSignIn}>Sign In</button>
+    </div>
+  );
+};
+
+export default LoginPage;
